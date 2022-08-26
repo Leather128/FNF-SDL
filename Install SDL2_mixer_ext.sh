3ce5f2f6f79d26cd09fc93aa9adbfb40b286c565
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-08-26 18:23:31 -0400

    fix installer

--- Install SDL2_mixer_ext.sh
+++ Install SDL2_mixer_ext.sh
@@ -19,3 +19,9 @@ sudo make install # Install the built library and headers into the system with s
 cd /tmp # go back to tmp
 
 sudo rm -rf /tmp/mixer_ext # remove all files relating to it
+
+# copy the library files to /usr/lib because they start in /usr/local/lib (not detected by the game :skull:)
+sudo cp /usr/local/lib/libSDL2_mixer_ext.a /usr/lib/libSDL2_mixer_ext.a
+sudo cp /usr/local/lib/libSDL2_mixer_ext.so /usr/lib/libSDL2_mixer_ext.so
+sudo cp /usr/local/lib/libSDL2_mixer_ext.so.2 /usr/lib/libSDL2_mixer_ext.so.2
+sudo cp /usr/local/lib/libSDL2_mixer_ext.so.2.6.0.0 /usr/lib/libSDL2_mixer_ext.so.2.6.0.0
